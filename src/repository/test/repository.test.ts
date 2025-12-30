e5d2d3c110d7bf92b15633e1b4a41413321e77d4
George Lopez <george.benjamin.lopez@gmail.com>
2025-12-30 13:01:57 +0000

    fix: Cleanup

--- src/repository/test/repository.test.ts
+++ src/repository/test/repository.test.ts
@@ -11,6 +11,7 @@ import { db } from "@/db/db";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 beforeAll(async () => {
+  console.log("NODE_ENV", process.env.NODE_ENV);
   migrate(db, { migrationsFolder: "./drizzle" });
 });
 
