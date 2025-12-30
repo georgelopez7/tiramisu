2b41277fbc411876a3657f8851cb4ced1bfbe112
George Lopez <george.benjamin.lopez@gmail.com>
2025-12-30 13:52:06 +0000

    feat: cleanup

--- setup.ts
+++ setup.ts
@@ -1,7 +1,4 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { db } from "@/db/db";
-import path from "path";
 
-migrate(db, {
-  migrationsFolder: path.resolve("./drizzle"),
-});
+migrate(db, { migrationsFolder: "./drizzle" });
